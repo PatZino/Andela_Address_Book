03c1c02e2b9fb151327a92ec98590a777ebaf448
logan wu & jenna navratil <student@epicodus.com>
2015-09-14 11:54:24 -0700

    add .remove() function to remove empty new address boxes each time user submits a contact and change class names

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
     $("#add-address").click(function() {
-      $("#new-addresses").append('<div class="new-address">' +
+      $("#new-addresses").append('<div class="new-address address">' +
                                    '<div class="form-group">' +
-                                     '<label for="new-street">Street</label>' +
-                                     '<input type="text" class="form-control new-street">' +
+                                       '<label for="new-street">Street</label>' +
+                                       '<input type="text" class="form-control new-street">' +
                                    '</div>' +
                                    '<div class="form-group">' +
-                                     '<label for="new-city">City</label>' +
-                                     '<input type="text" class="form-control new-city">' +
+                                       '<label for="new-city">City</label>' +
+                                       '<input type="text" class="form-control new-city">' +
                                    '</div>' +
                                    '<div class="form-group">' +
-                                     '<label for="new-state">State</label>' +
-                                     '<input type="text" class="form-control new-state">' +
+                                       '<label for="new-state">State</label>' +
+                                       '<input type="text" class="form-control new-state">' +
                                    '</div>' +
                                  '</div>');
     });
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
         var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses:  [] };
 
-        $(".new-address").each(function() {
+        $(".address").each(function() {
             var inputtedStreet = $(this).find("input.new-street").val();
             var inputtedCity = $(this).find("input.new-city").val();
             var inputtedState = $(this).find("input.new-state").val();
@@ -34,7 +34,7 @@ $(document).ready(function() {
         });
 
         $("ul#contacts").append("<li><span class='contact'>" +
-                                newContact.firstName + 
+                                newContact.firstName +
                                 "</span></li>");
 
         $("input#new-first-name").val("");
@@ -42,6 +42,9 @@ $(document).ready(function() {
         $("input.new-street").val("");
         $("input.new-city").val("");
         $("input.new-state").val("");
+
+        // $(".new-address").css("background-color", "red");
+        $(".new-address").remove();
 
         $(".contact").last().click(function() {
             $("#show-contact").show();
